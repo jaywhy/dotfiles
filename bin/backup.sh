3e99049373b5988c16221cb9be8e22810b956fbf
Jason Yates <jason@jasonyates.me>
2022-03-15 10:56:23 -0400

    I'm not using Time Machine anymore. Using an rsync alternative.

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,6 +1,10 @@
 cd $HOME
 /opt/homebrew/bin/brew bundle dump --file=~/OneDrive/Backup/Brewfile --force
+pg_dumpall > ~/OneDrive/Backup/postgresql_backup.db
 
-rsync -avh ~/OneDrive/NaturaLawn/File\ Cabinet jason@192.168.5.21:/volume1/Backup/
-rsync -avh ~/OneDrive/Personal\ File\ Cabinet jason@192.168.5.21:/volume1/Backup/
-rsync -avh ~/OneDrive/Yates\ File\ Cabinet jason@192.168.5.21:/volume1/Backup/
+rsync -ah --info=progress2 ~/OneDrive/NaturaLawn/File\ Cabinet jason@192.168.5.21:/volume1/Backup/
+rsync -ah --info=progress2 ~/OneDrive/Personal\ File\ Cabinet jason@192.168.5.21:/volume1/Backup/
+rsync -ah --info=progress2 ~/OneDrive/Yates\ File\ Cabinet jason@192.168.5.21:/volume1/Backup/
+
+cd $(dirname "$0")
+./rsync_tmbackup.sh /Users/jason/ jason@192.168.5.21:/volume1/Backup/TimeMachine/ excluded_files.txt
