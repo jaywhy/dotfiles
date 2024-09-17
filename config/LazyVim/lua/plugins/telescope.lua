0f767463067d5310656a13d26644c5c87bef82fc
Jason Yates <jason@jasonyates.me>
2024-09-17 00:35:26 -0400

    Using fzf with telescope

---
+++ config/LazyVim/lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+-- Faster telescope search with fzf
+return {
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+}
