815b8a3a9e9ce6c7a378e857079ef0f5551a36ab
Jason Yates <jason@jasonyates.me>
2024-09-25 22:12:56 -0400

    Changed font

--- wezterm.lua
+++ wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.font = wezterm.font("Hack Nerd Font Mono")
+config.font = wezterm.font("Hack Nerd Font")
 config.font_size = 19
 
 config.window_decorations = "RESIZE"
