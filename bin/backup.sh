89ab034593942532eaad13f1a4c5bb01c6b1e980
Jason Yates <jason@jasonyates.me>
2025-10-27 15:27:54 -0400

    changed excludes

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,25 +1,5 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-export RESTIC_PASSWORD_COMMAND="security find-generic-password -s restic-backup-password -w"
-BACKUP_ROOT="/Users/jason/Backup"
 
-# brew bundle dump --file="$BACKUP_ROOT/Brewfile" --force
-# /Applications/Postgres.app/Contents/Versions/latest/bin/pg_dumpall --quote-all-identifiers | gzip >"$BACKUP_ROOT/postgres.sql.gz"
-
-backup_directory() {
-  local source_dir="$1"
-
-  REPO="sftp:jason@192.168.5.21:/Backup/restic"
-  EXCLUDES="$HOME/.config/restic/excludes.txt"
-
-  echo "Backing up $source_dir"
-  restic -r "$REPO" backup "$source_dir" --exclude-file "$EXCLUDES" --tag daily
-}
-
-backup_directory "/Users/jason"
-# backup_directory "/Users/jason/Documents/Personal File Cabinet"
-# backup_directory "/Users/jason/Documents/Yates File Cabinet"
-# backup_directory "/Users/jason/Library/CloudStorage/GoogleDrive-jason@nlawilm.com/Shared drives/NaturaLawn/File Cabinet"
-# backup_directory "/Users/jason/Notes"
-
-restic -r "$REPO" forget --keep-daily 7 --keep-weekly 4 --keep-monthly 6 --prune
+brew bundle dump --file="$BACKUP_ROOT/Brewfile" --force
+/Applications/Postgres.app/Contents/Versions/latest/bin/pg_dumpall --quote-all-identifiers | gzip >"$BACKUP_ROOT/postgres.sql.gz"
