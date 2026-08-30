d98aa20260b6faba50b82ec3c257a70569b69752
Jason Yates <jason@jasonyates.me>
2026-08-30 14:04:35 -0400

    backup: point BACKUP_ROOT at ~/Documents/Backup
    No longer using OneDrive.

--- bin/backup.sh
+++ bin/backup.sh
@@ -1,10 +1,8 @@
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
-BACKUP_ROOT="/Users/jason/OneDrive/Backup"
+# ~/Documents is synced to the Synology via Syncthing
+BACKUP_ROOT="$HOME/Documents/Backup"
 mkdir -p "$BACKUP_ROOT"
 
 brew bundle dump --file="$BACKUP_ROOT/Brewfile" --force
 /Applications/Postgres.app/Contents/Versions/latest/bin/pg_dumpall --quote-all-identifiers | gzip >"$BACKUP_ROOT/postgres.sql.gz"
-
-bash backup_directory.sh "$BACKUP_ROOT" "/Users/jason/code" "/Users/jason/code/source"
-bash backup_directory.sh "$BACKUP_ROOT" "/Users/jason/Notes"
